ed37d4497c1aa2557079651f80de4b2d71ac16d4
Nils Knappmeier <npm@knappi.org>
2022-12-11 22:05:57 +0100

    fix "test"-error in vite.config by upgrading to viest 0.25.7

--- packages/parser-ng/vite.config.ts
+++ packages/parser-ng/vite.config.ts
@@ -1,20 +1,24 @@
-/// <reference types="vitest/" />
+/// <reference types="vitest" />
 
+import {resolve} from 'path'
 import {defineConfig} from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
     plugins: [dts()],
     build: {
-        lib: {
-            entry: "src/index.ts",
-        },
         outDir: "dist",
+        lib: {
+            entry: {
+                "spec": resolve(__dirname, 'src', '__generated__', "spec.ts"),
+            },
+            formats: ['es', "cjs"],
+        },
         sourcemap: true,
         minify: false,
         target: "esnext"
     },
     test: {
         globals: true
-    },
+    }
 })
