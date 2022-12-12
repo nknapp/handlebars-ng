3c88b2380312662ec738493f62bd8b414abcee69
Nils Knappmeier <npm@knappi.org>
2022-12-12 22:27:11 +0100

    first performance tests

--- packages/@handlebars-ng/benchmarks/src/index.ts
+++ packages/@handlebars-ng/benchmarks/src/index.ts
@@ -2,32 +2,23 @@ import {
   FunctionBenchmark,
   FunctionUnderTest,
 } from "./testbench/FunctionBenchmark";
+import { parse } from "@handlebars-ng/parser";
+import Handlebars from "handlebars";
 
-const uintArrayUnderTest = new Uint8Array(100000);
-const stringUnderTest = "1234567890".repeat(10000);
-
-function typedArray() {
-  let lastValue = 0;
-  for (const x of uintArrayUnderTest) {
-    lastValue = x;
-  }
-  return lastValue;
+function originalHandlebars() {
+  Handlebars.parse("this is {{a}} {{test}}");
 }
 
-function string() {
-  let lastValue = "";
-  for (const x of stringUnderTest) {
-    lastValue = x;
-  }
-  return lastValue;
+function handlebarsNg() {
+  parse("this is {{a}} {{test}}");
 }
 
-await runWith(typedArray);
-await runWith(string);
+await runWith(originalHandlebars);
+await runWith(handlebarsNg);
 
 async function runWith(fn: FunctionUnderTest): Promise<void> {
   const bench = new FunctionBenchmark(fn);
-  await bench.run(1000);
+  await bench.run(10000);
   // eslint-disable-next-line no-console
   console.log(fn.name, bench.getStats());
 }
--- packages/@handlebars-ng/parser/vite.config.ts
+++ packages/@handlebars-ng/parser/vite.config.ts
@@ -1,24 +1,25 @@
 /// <reference types="vitest" />
 
-import {resolve} from 'path'
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-    plugins: [dts()],
-    build: {
-        outDir: "dist",
-        lib: {
-            entry: {
-                "spec": resolve(__dirname, 'src', '__generated__', "spec.ts"),
-            },
-            formats: ['es', "cjs"],
-        },
-        sourcemap: true,
-        minify: false,
-        target: "esnext"
+  plugins: [dts({ include: "src" })],
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: {
+        index: "src/index.ts",
+      },
+      formats: ["es", "cjs"],
+      name: "handlebars-parser",
     },
-    test: {
-        globals: true
-    }
-})
+
+    sourcemap: true,
+    minify: false,
+    target: "esnext",
+  },
+  test: {
+    globals: true,
+  },
+});
