a57df06f386eb9d5d5e359692019c3721a319b19
Nils Knappmeier <npm@knappi.org>
2022-12-23 15:06:06 +0100

    desktop version

--- packages/@handlebars-ng/specification/src/components/Sidebar/index.tsx
+++ packages/@handlebars-ng/specification/src/components/Sidebar/index.tsx
@@ -1,10 +1,10 @@
 import { showSidebar, toggleSidebar } from "@/store/sidebar";
 import { useStore } from "@nanostores/solid";
-import type { Component, createSignal, JSXElement } from "solid-js";
+import type { Component, JSXElement } from "solid-js";
 
 export const Sidebar: Component<{ children?: JSXElement }> = (props) => {
-  const aside = "absolute w-64 p-4 bg-orange-100 transition h-full";
-  const translateLeft = "transform -translate-x-64 duration-300";
+  const aside = "absolute w-64 p-4 bg-orange-100 transition h-full drop-shadow-lg lg:filter-none";
+  const translateLeft = "transform -translate-x-64 duration-300 lg:translate-x-0";
 
   const show = useStore(showSidebar);
 
--- packages/@handlebars-ng/specification/src/components/SidebarButton.tsx
+++ packages/@handlebars-ng/specification/src/components/SidebarButton.tsx
@@ -7,7 +7,7 @@ export const SidebarButton: Component = () => {
   const rotated = "transform rotate-90"
   const baseStyle = "transition duration-300"
   return (
-      <button class="border border-1 white block " onClick={toggleSidebar}>
+      <button class="border border-1 white block lg:hidden" onClick={toggleSidebar}>
         <svg width="24" height="24" class={baseStyle + " " + (sideBarVisible() ? rotated : "")}>
         <line x1="4" y1="6" x2="20" y2="6" stroke="currentColor" />
         <line x1="4" y1="12" x2="20" y2="12" stroke="currentColor" />
