c14908c74b7f44951b85410f4e2033d47f1afbb8
Nils Knappmeier <npm@knappi.org>
2022-12-23 19:26:22 +0100

    license information

--- packages/@handlebars-ng/specification/types/ast.d.ts
+++ packages/@handlebars-ng/specification/types/ast.d.ts
@@ -1,3 +1,17 @@
+/* These definitions were originally imported from https://github.com/DefinitelyTyped/DefinitelyTyped
+ * and may include contributions from the DefinitelyTyped community by:
+ *   - Albert Willemsen <https://github.com/AlbertWillemsen-Centric>
+ *   - Boris Yankov <https://github.com/borisyankov>
+ *   - Jessica Franco <https://github.com/Kovensky>
+ *   - Masahiro Wakame <https://github.com/vvakame>
+ *   - Raanan Weber <https://github.com/RaananW>
+ *   - Sergei Dorogin <https://github.com/evil-shrike>
+ *   - webbiesdk <https://github.com/webbiesdk>
+ *   - Andrew Leedham <https://github.com/AndrewLeedham>
+ *   - Nils Knappmeier <https://github.com/nknapp>
+ * For full history prior to their migration to handlebars.js, please see:
+ * https://github.com/DefinitelyTyped/DefinitelyTyped/commits/1ce60bdc07f10e0b076778c6c953271c072bc894/types/handlebars/index.d.ts
+ */
 export interface Program extends Node {
   type: "Program";
   body: Statement[];
