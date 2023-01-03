7fdfe4bcb80f43a8cdd3c85f8000f1e3b6fede76
Nils Knappmeier <npm@knappi.org>
2023-01-03 15:44:29 +0100

    fix layout

--- packages/handlebars-ng.knappi.org/src/layouts/default/Sidebar.tsx
+++ packages/handlebars-ng.knappi.org/src/layouts/default/Sidebar.tsx
@@ -3,7 +3,7 @@ import { useStore } from "@nanostores/solid";
 import type { Component, JSXElement } from "solid-js";
 
 export const Sidebar: Component<{ children?: JSXElement }> = (props) => {
-  const aside = "absolute w-64 p-4 bg-orange-100 transition h-full drop-shadow-lg lg:filter-none";
+  const aside = "w-64 p-4 bg-orange-100 transition h-full drop-shadow-lg lg:filter-none";
   const translateLeft = "transform -translate-x-64 duration-300 lg:translate-x-0";
 
   const show = useStore(showSidebar);
