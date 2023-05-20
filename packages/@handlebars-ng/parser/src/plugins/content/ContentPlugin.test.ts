283e1611d7012e5779e45dcc26e7b7273a7cfd57
Nils Knappmeier <npm@knappi.org>
2023-05-20 23:51:18 +0200

    add escaped mustaches

--- packages/@handlebars-ng/parser/src/plugins/content/ContentPlugin.test.ts
+++ packages/@handlebars-ng/parser/src/plugins/content/ContentPlugin.test.ts
@@ -1,6 +1,7 @@
 import { createHandlebarsParser } from "../../core/createHandlebarsParser";
 import { ContentPlugin } from "./ContentPlugin";
 import { loc } from "../../test-utils/loc";
+import { HandlebarsParserPlugin } from "../../core/types";
 
 describe("ContentPlugin", () => {
   it("parses content", () => {
@@ -15,4 +16,25 @@ describe("ContentPlugin", () => {
       },
     ]);
   });
+
+  it("parses escaped mustaches", () => {
+    const parser = createHandlebarsParser({
+      plugins: [ContentPlugin, TestMustache],
+    });
+    const ast = parser.parse("a\\{{c");
+    expect(ast.body).toEqual([
+      {
+        type: "ContentStatement",
+        loc: loc("1:0-1:5"),
+        original: "a\\{{c",
+        value: "a{{c",
+      },
+    ]);
+  });
 });
+
+const TestMustache: HandlebarsParserPlugin = {
+  statement(statementRegistry) {
+    statementRegistry.addMatchRule({ type: "OPEN", match: "{{" });
+  },
+};
--- packages/@handlebars-ng/parser/src/plugins/content/ContentPlugin.ts
+++ packages/@handlebars-ng/parser/src/plugins/content/ContentPlugin.ts
@@ -2,21 +2,32 @@ import { HandlebarsParserPlugin, ParseContext } from "../../core/types";
 import { ContentStatement } from "@handlebars-ng/abstract-syntax-tree";
 import { tok } from "../../core/utils/tok";
 
-export const TOK_CONTENT = tok("CONTENT");
+export const TOK_CONTENT = tok("CONTENT", "ESCAPED_MUSTACHE");
 
 export const ContentPlugin: HandlebarsParserPlugin = {
   statement(registry) {
     registry.setFallbackRule({ type: "CONTENT", lineBreaks: true });
+    registry.addMatchRule({
+      type: "ESCAPED_MUSTACHE",
+      match: "\\{{",
+      value: () => "{{",
+    });
     registry.addParser<ContentStatement>(TOK_CONTENT, parseContentStatement);
   },
 };
 
 export function parseContentStatement(context: ParseContext): ContentStatement {
-  const token = context.tokens.eat(TOK_CONTENT);
+  const tokens = [...context.tokens.keepEating(TOK_CONTENT)];
+
+  const firstToken = tokens[0];
+  const lastToken = tokens[tokens.length - 1];
+  const combinedValues = tokens.map((token) => token.value).join("");
+  const combinedOriginals = tokens.map((token) => token.original).join("");
+
   return {
     type: "ContentStatement",
-    loc: context.tokens.location(token, token),
-    value: token.value,
-    original: token.original,
+    original: combinedOriginals,
+    loc: context.tokens.location(firstToken, lastToken),
+    value: combinedValues,
   };
 }
