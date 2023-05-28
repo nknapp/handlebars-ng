41d9dd6b5c0f2f35c987f6058f4ce6f46206230c
Nils Knappmeier <npm@knappi.org>
2023-05-28 23:22:03 +0200

    playground: remove non-null assertion

--- packages/@handlebars-ng/playground/src/showcase.tsx
+++ packages/@handlebars-ng/playground/src/showcase.tsx
@@ -4,10 +4,10 @@ import "./index.css";
 
 const root = document.getElementById("root");
 
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
+if (root == null || !(root instanceof HTMLElement)) {
   throw new Error(
     "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got mispelled?"
   );
 }
 
-render(() => <Playground />, root!);
+render(() => <Playground />, root);
